5229e5dab6eab004c9b6f812a8cfae0e6de6b075
Rory <webmaster@cptc.edu>
2015-06-04 11:14:14 -0700

    added favicons

--- sites/all/themes/boo/html.tpl.php
+++ sites/all/themes/boo/html.tpl.php
@@ -59,6 +59,34 @@
 
      <style type="text/css" media="all">
 </style> 
+
+<?php 
+global $base_url;
+$faviconpath = $base_url . "/" . path_to_theme();
+?>
+
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo $faviconpath; ?>/images/favicons/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $faviconpath; ?>/images/favicons/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $faviconpath; ?>/images/favicons/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $faviconpath; ?>/images/favicons/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php echo $faviconpath; ?>/images/favicons/apple-touch-icon-60x60.png" />
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo $faviconpath; ?>/images/favicons/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo $faviconpath; ?>/images/favicons/favicons/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo $faviconpath; ?>/images/favicons/apple-touch-icon-152x152.png" />
+<link rel="icon" type="image/png" href="<?php echo $faviconpath; ?>/images/favicons/favicon-196x196.png" sizes="196x196" />
+<link rel="icon" type="image/png" href="<?php echo $faviconpath; ?>/images/favicons/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/png" href="<?php echo $faviconpath; ?>/images/favicons/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="<?php echo $faviconpath; ?>/images/favicons/favicon-16x16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="<?php echo $faviconpath; ?>/images/favicons/favicon-128.png" sizes="128x128" />
+<meta name="application-name" content="&nbsp;"/>
+<meta name="msapplication-TileColor" content="#FFFFFF" />
+<meta name="msapplication-TileImage" content="<?php echo $faviconpath; ?>/images/favicons/mstile-144x144.png" />
+<meta name="msapplication-square70x70logo" content="<?php echo $faviconpath; ?>/images/favicons/mstile-70x70.png" />
+<meta name="msapplication-square150x150logo" content="<?php echo $faviconpath; ?>/images/favicons/mstile-150x150.png" />
+<meta name="msapplication-wide310x150logo" content="<?php echo $faviconpath; ?>/images/favicons/mstile-310x150.png" />
+<meta name="msapplication-square310x310logo" content="<?php echo $faviconpath; ?>/images/favicons/mstile-310x310.png" />
+
+
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
